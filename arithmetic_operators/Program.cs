4874dbda7377e365fc4bbe7a3656054d9df91138
kuzomix <yurisa331007@gmail.com>
2025-07-20 15:07:51 +0800

    2025/07/20完成user_input和arithmetic_operators兩個章節

---
+++ arithmetic_operators/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace arithmetic_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int friends = 10;
+            //double friends = 5;
+
+            //讓friends增加的不同寫法
+            //friends = friends + 2;
+            //friends += 2; //簡寫
+            //friends++; //每次增加1
+
+            //讓friends減少的不同寫法
+            //friends = friends - 1;
+            //friends -= 1; //簡寫
+            //friends--; //每次減1
+
+            //讓friends 的值乘以 2
+            //friends = friends * 2;
+            //friends *= 2;
+
+            //讓friends 的值除以 2，整數除法會去掉小數
+            //friends = friends / 2;
+            //friends /= 2;
+
+            //%取餘數，remainder會儲存這個結果
+            int remainder = friends % 2;
+
+            //Console.WriteLine(friends);
+            Console.WriteLine(remainder);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ user_input/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?"); //主控台顯示文字
+            String name = Console.ReadLine(); //等待使用者輸入名字，並把輸入的內容存到 name 這個字串變數中
+
+            Console.WriteLine("What's your age?");
+            int age = Convert.ToInt32(Console.ReadLine()); //等待使用者輸入年齡（字串形式），把它轉換成整數，存到 age 變數中
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("You are " + age + " years old");
+
+            Console.ReadKey();
+
+        }
+    }
+}
