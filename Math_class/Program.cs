d52cebf97816f20ca3a2a03f8ac30599c3f52cf3
kuzomix <yurisa331007@gmail.com>
2025-07-21 22:33:11 +0800

    2025/07/21完成Math_class和random_numbers兩個章節

---
+++ Math_class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace Math_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3;
+            double y = 5;
+
+            double a = Math.Pow(x, 3); //x的3次方
+            //double b = Math.Sqrt(x); //x的平方根
+            //double c = Math.Abs(x); //取絕對值x
+            //double d = Math.Round(x); //四捨五入x(3.0->3)
+            //double e = Math.Ceiling(x); //無條件進位(天花板函數)
+            //double f = Math.Floor(x); //無條件捨去(地板函數)
+            //double g = Math.Max(x, y); // 取兩數中較大的值
+            //double h = Math.Min(x, y); // 取兩數中較小的值
+
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ random_numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace random_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //模擬擲骰子，每次執行結果通常會不同
+            Random random = new Random(); //產生隨機數字
+                                          //Random類別再沒有指定種子情況下，會使用系統時間(毫秒)作為種子，所以每次執行程式產生的亂數序列都會不同
+
+            //產生介於1~6之間的整數(不含7)，三行分別產生三個獨立的骰子點數
+            int num1 = random.Next(1, 7);
+            int num2 = random.Next(1, 7);
+            int num3 = random.Next(1, 7);
+
+            //double num = random.NextDouble(); //產生介於0.0~1.0之間的浮點數不含1.0)
+
+            //如果想讓每次執行結果都一樣
+            //Random random = new Random(123); //固定種子
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+
+            Console.ReadKey();
+
+        }
+    }
+}
