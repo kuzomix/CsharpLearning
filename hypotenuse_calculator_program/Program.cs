39ab72f3572519d0f24aa2993a79598e34b16669
kuzomix <yurisa331007@gmail.com>
2025-07-22 13:47:14 +0800

    2025/07/22完成hypotenuse_calculator_program和string_methods兩個章節

---
+++ hypotenuse_calculator_program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace hypotenuse_calculator_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter side A: "); //提示使用者輸入第一個直角邊
+            double a = Convert.ToDouble(Console.ReadLine()); //讀取使用者輸入的字串並轉換成double儲存
+
+            Console.WriteLine("Enter side B: "); //提示使用者輸入另一個直角邊
+            double b = Convert.ToDouble(Console.ReadLine()); 
+
+            double c = Math.Sqrt((a * a) + (b * b)); //畢氏定理c=根號a平方+b平方，Math.Sqrt計算平方根
+
+            Console.WriteLine("The hypotenuse is: " + c); //得到斜邊長度
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ string_methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace string_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //兩個字串變數宣告
+            String fullName = "Bro Code";
+            String phoneNumber = "123-456-7890";
+
+            fullName = fullName.ToUpper(); //將fullName內容轉換成全大寫
+            //fullName = fullName.ToLower(); //將fullName內容轉換成全小寫
+            Console.WriteLine(fullName);
+
+            phoneNumber = phoneNumber.Replace("-", ""); //使用Replace方法將phoneNumber中所有"-"字元移除
+            Console.WriteLine(phoneNumber);
+
+            String userName = fullName.Insert(0,"Mr."); //使用Insert方法將fullName開頭(索引0)插入"Mr."
+            Console.WriteLine(userName);
+
+            Console.WriteLine(fullName.Length); //輸出fullName長度(字元數)，包含空格
+
+            String firstName = fullName.Substring(0, 3); //使用Substring方法擷取從索引0開始的3個字元
+            String lastName = fullName.Substring(4, 4); //擷取從索引4開始的4個字元(跳過空格)
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+
+            Console.ReadKey();
+
+        }
+    }
+}
