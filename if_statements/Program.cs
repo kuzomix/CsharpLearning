08f78b615e022325da9f93d0dd59559792eaa77c
kuzomix <yurisa331007@gmail.com>
2025-07-23 16:34:05 +0800

    2025/07/23完成if_statements和switches兩個章節

---
+++ if_statements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace if_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if statement = a basic form of decision making
+
+            Console.WriteLine("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //讀取使用者輸入，轉換為整數型別 int，儲存到變數 age 中，根據輸入的數字範圍不同會顯示不同文字
+            if (age > 100)
+            {
+                Console.WriteLine("You are too old to sign up!");
+            }
+            else if (age >= 18)
+            {
+                Console.WriteLine("You are now signed up!");
+            }
+            else if(age < 0)
+            {
+                Console.WriteLine("You haven't been born yet!");
+            }
+            else
+            {
+                Console.WriteLine("You must be 18+ to sign up!");
+            }
+
+            //讀取使用者輸入文字，儲存到變數 name 中
+            Console.WriteLine("Please enter your name: ");
+            String name = Console.ReadLine();
+
+            if (name != "")
+            {
+                Console.WriteLine("Hello " + name);
+            }
+            else
+            {
+                Console.WriteLine("You did not enter your name!");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ switches/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace switches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //switch = an efficient alternative to many else if statements
+
+            /*Console.WriteLine("What day is it today?");
+            String day = Console.ReadLine();
+
+            if (day == "Monday")
+            {
+                Console.WriteLine("It's Monday!");
+            }
+            else if (day == "Tuesday")
+            {
+                Console.WriteLine("It's Tuesday!");
+            }
+            else if (day == "Wednesday")
+            {
+                Console.WriteLine("It's Wednesday!");
+            }
+            else if (day == "Thursday")
+            {
+                Console.WriteLine("It's Thursday!");
+            }
+            else if ( day == "Friday")
+            {
+                Console.WriteLine("It's Friday!");
+            }
+            else if (day == "Saturday")
+            {
+                Console.WriteLine("It's Saturday!");
+            }
+            else if (day == "Sunday")
+            {
+                Console.WriteLine("It's Sunday!");
+            }
+            else
+            {
+                Console.WriteLine(day + " is not a day!");
+        */
+       
+        
+
+        Console.WriteLine("What day is it today?");
+            String day = Console.ReadLine();
+
+            switch (day)
+            {
+                case "Monday":
+                    Console.WriteLine("It's Monday!");
+                    break;
+                case "Tuesday":
+                    Console.WriteLine("It's Tuesday!");
+                    break;
+                case "Wednesday":
+                    Console.WriteLine("It's Wednesday!");
+                    break;
+                case "Thursday":
+                    Console.WriteLine("It's Thursday!");
+                    break;
+                case "Friday":
+                    Console.WriteLine("It's Friday!");
+                    break;
+                case "Saturday":
+                    Console.WriteLine("It's Saturday!");
+                    break;
+                case "Sunday":
+                    Console.WriteLine("It's Sunday!");
+                    break;
+                default:
+                    Console.WriteLine(day + " is not a day!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
